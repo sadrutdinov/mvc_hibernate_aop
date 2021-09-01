2c9c4b263a353db31f6702a11bd169d342acb45b
Садрутдинов Айдар <a.i.sadrutdinov@kalashnikovconcern.ru>
2021-09-01 14:49:37 +0400

    lesson 71. Аннотация @Service

--- src/main/java/com/sai/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/com/sai/spring/mvc_hibernate_aop/controller/MyController.java
@@ -1,7 +1,7 @@
 package com.sai.spring.mvc_hibernate_aop.controller;
 
-import com.sai.spring.mvc_hibernate_aop.dao.EmployeeDAO;
 import com.sai.spring.mvc_hibernate_aop.entity.Employee;
+import com.sai.spring.mvc_hibernate_aop.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,12 +13,12 @@ import java.util.List;
 public class MyController {
 
     @Autowired
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     @RequestMapping("/")
     public String showAllEmployees(Model model) {
 
-        List<Employee> allEmployees = employeeDAO.getAllEmployees();
+        List<Employee> allEmployees = employeeService.getAllEmployees();
 
         model.addAttribute("allEmployees", allEmployees);
 
--- src/main/java/com/sai/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
+++ src/main/java/com/sai/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -16,7 +16,6 @@ public class EmployeeDAOImpl implements EmployeeDAO{
     private SessionFactory sessionFactory;
 
     @Override
-    @Transactional
     public List<Employee> getAllEmployees() {
         Session session = sessionFactory.getCurrentSession();
 
